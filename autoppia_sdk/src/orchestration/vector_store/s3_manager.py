359cf9d5dc1bc16782801a8d86b55dff1eef270a
rightpath3721 <rightpath.dev@gmail.com>
2025-01-13 00:50:03 +0200

    update

--- autoppia_sdk/src/orchestration/vector_store/s3_manager.py
+++ autoppia_sdk/src/orchestration/vector_store/s3_manager.py
@@ -1,22 +1,24 @@
 import boto3
-from django.conf import settings
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 
 class S3Manager:
     def __init__(self):
         self.s3_client = boto3.client(
             "s3",
-            aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-            aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+            aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+            aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
         )
 
     def uploadFile(self, file_path, key):
-        self.s3_client.upload_file(file_path, settings.AWS_STORAGE_BUCKET_NAME, key)
+        self.s3_client.upload_file(file_path, os.getenv("AWS_STORAGE_BUCKET_NAME"), key)
 
     def downloadFile(self, key, destination_path):
         self.s3_client.download_file(
-            settings.AWS_STORAGE_BUCKET_NAME, key, destination_path
+            os.getenv("AWS_STORAGE_BUCKET_NAME"), key, destination_path
         )
 
     def createFolder(self, folder_name):
-        self.s3_client.put_object(Bucket=settings.S3_BUCKET_NAME, Key=f"{folder_name}/")
+        self.s3_client.put_object(Bucket=os.getenv("AWS_STORAGE_BUCKET_NAME"), Key=f"{folder_name}/")
