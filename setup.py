40921f3a6a978feb4a5efb46a93e9bc2fe5ce806
rightpath3721 <rightpath.dev@gmail.com>
2025-01-12 21:55:32 +0200

    update

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import find_packages, setup
 
-with open("requirements.txt", "r") as f:
+with open("exact_requirements.txt", "r") as f:  
     required = f.read().splitlines()
 
 setup(
     name="autoppia_sdk",
     version="0.1",
     packages=find_packages(),
-    install_requires=required
+    install_requires=[
+        req.strip() for req in required 
+        if req.strip() and not req.startswith('#') and not req.startswith('-')
+    ]
 )
